c9c7b25eaa2821fcf78e8b640bb40eb343535756
estefanoshin <estefanoshin@yahoo.com.ar>
2019-06-09 23:51:15 -0300

    oh sh...

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,9 +4,8 @@
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_tpcpp_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
-
-
+}
